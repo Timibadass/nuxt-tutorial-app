56602b519afe5ca0b51f6978d8333375faac8407
Timibadass <oblacishere@gmail.com>
2020-04-06 20:47:42 +0100

    Add nav component

---
+++ components/navBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="nav">
+    <div class="logo__container">
+      <Logo />
+    </div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/About">About</nuxt-link>
+  </nav>
+</template>
+
+<script>
+import Logo from "@/components/Logo";
+export default {
+  name: "nav",
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,28 @@
 <template>
   <div class="container">
+    <Nav />
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-tutorial-app
-      </h1>
-      <h2 class="subtitle">
-        My outstanding Nuxt.js project
-      </h2>
+      <h1 class="title">nuxt-tutorial-app</h1>
+      <h2 class="subtitle">My outstanding Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
+import Nav from "~/components/navBar.vue";
 
 export default {
   components: {
-    Logo
+    Logo,
+    Nav
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +36,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
