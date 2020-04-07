bd3ff20e16cf35f6274f80e487c1c9c7a6bdf52f
Timibadass <oblacishere@gmail.com>
2020-04-07 03:14:21 +0100

    add  dynamic page for directory info

---
+++ pages/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="directory" v-if="directory">
+    <h1 class="directory__name">{{ directory.name }}</h1>
+    <p class="directory__info">{{ directory.info }}</p>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "directory-info",
+  computed: {
+    ...mapState(["directory"])
+  }
+};
+</script>
+
+<style></style>
